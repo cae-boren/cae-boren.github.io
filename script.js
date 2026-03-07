5a155a19ce5c146633372e0021a67fd8196e217b
kiwi <henrbore@playgroundsquad.com>
2026-03-07 21:15:44 +0100

    Remade the entire website >.<

--- script.js
+++ script.js
@@ -1,55 +1,72 @@
-var body           = document.getElementById("body");
-var header         = document.getElementById("header");
-var welcome_text   = document.getElementById("welcome-text");
-var header_texts   = document.getElementsByClassName("nav-text");
-var project_images = document.getElementsByClassName("project-image");
-var project_infos  = []
+const name_button      = document.getElementById('name-button')
+const welcome_card     = document.getElementById('welcome-card')
+const nav_buttons      = document.querySelectorAll('header nav a')
+const project_images   = document.getElementsByClassName("project-image")
+const section_contents = document.querySelectorAll('.section-content')
+const project_contents = document.querySelectorAll('.m0ther, .entropy, .slip-up-n-slide, .terrain-tool, .personal-project, .about-me')
+
+Array.from(project_images).forEach(img => {
+    img.addEventListener("dragstart", (e) => e.preventDefault())
+})
+
+name_button.addEventListener("click", () => {
+    hide_all_section_contents()
+
+    welcome_card.classList.add('visible')
+    welcome_card.classList.remove('hidden')
+})
+
+nav_buttons.forEach(button => {
+    if (button.innerHTML == "LINKEDIN" || button.innerHTML == "EMAIL" || button.innerHTML == "CV") {
+        return
+    }
+    
+    button.addEventListener("click", () => {
+        welcome_card.classList.add('hidden')
+        hide_all_section_contents()
+
+        const target = button.dataset.target
+        if (target == "about-me") {
+            document.querySelectorAll('.about-me').forEach(s => {
+                s.classList.add('visible')
+                s.classList.remove('hidden')
+            })
+        } else {
+            document.getElementById(target).classList.add('visible')
+            document.getElementById(target).classList.remove('hidden')
+        }
+    })
+})
 
 document.addEventListener("DOMContentLoaded", () => {
-  for (let index = 0; index < project_images.length; index++) {
-    const element = project_images[index];
-    element.addEventListener("mouseover", (event) => {
-        element.parentElement.lastElementChild.style.opacity = 1.0
-    })
-    element.addEventListener("mouseleave", (event) => {
-        element.parentElement.lastElementChild.style.opacity = 0.0
-    })
-    element.addEventListener("click", (event) => {
-        let split_strings = element.src.split("/")
-        let file_name = element.src.split("/")[split_strings.length - 1].split(".").slice(0, -1) + ".html"
-        document.location.href = "sub_pages/" + file_name
-    })
+    for (let index = 0; index < project_images.length; index++) {
+        const element = project_images[index];
+        
+        element.addEventListener("mouseover", (event) => {
+            element.parentElement.lastElementChild.style.opacity = 1.0
+        })
 
-    project_infos.push(element.parentElement.lastElementChild)
-  }
+        element.addEventListener("mouseleave", (event) => {
+            element.parentElement.lastElementChild.style.opacity = 0.0
+        })
 
-    check_window_size()
-});
+        element.addEventListener("click", (event) => {
+            hide_all_section_contents()
+            let split_strings = element.src.split("/")
+            let content_class = element.src.split("/")[split_strings.length - 1].split(".").slice(0, -1)
+            content_class = content_class[0].replace("_", "-").replace("_", "-").replace("_", "-")
 
-document.addEventListener("scroll", (event) => {
-    let scroll_y   = window.scrollY
-    
-    if (scroll_y == 0) {
-        welcome_text.style.opacity = 1
-    }
-    else {
-        welcome_text.style.opacity = 0
+            document.querySelectorAll('.' + content_class).forEach(s => s.classList.add('visible'))
+            document.querySelectorAll('.' + content_class).forEach(s => s.classList.remove('hidden'))
+        })
     }
 })
 
-addEventListener("resize", (event) => {
-    check_window_size()
-})
+function hide_all_section_contents() {
+    window.scrollTo(0, 0)
 
-function check_window_size() {
-    if (window.innerWidth < 1100) {
-        var path = window.location.pathname;
-        var page = path.split("/").pop();
-        if (page == "index.html") {
-            window.location.href = "sub_pages/too_narrow_screen.html";
-        }
-        else {
-            window.location.href = "../sub_pages/too_narrow_screen.html";
-        }
-    }
+    section_contents.forEach(s => s.classList.remove('visible'))
+    section_contents.forEach(s => s.classList.add('hidden'))
+    project_contents.forEach(s => s.classList.remove('visible'))
+    project_contents.forEach(s => s.classList.add('hidden'))
 }
